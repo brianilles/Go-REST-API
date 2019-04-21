08615ce8f2b7f05c780a2900875b57370bfb1e27
Brian Illes <brianrilles@gmail.com>
2019-04-20 19:49:37 -0700

    Adds GET /api/todos/:id

--- api/main.go
+++ api/main.go
@@ -9,9 +9,9 @@ import (
 )
 
 type Todo struct {
-	ID        string `json:"id`
+	ID        string `json:"id"`
 	Task      string `json:"task"`
-	Completed bool   `json:completed`
+	Completed bool   `json:"completed"`
 }
 
 var todos []Todo
@@ -21,12 +21,24 @@ func getTodos(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(todos)
 }
 
+func getTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range todos {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
 func main() {
 	todos = append(todos, Todo{ID: "1", Task: "Make an API with Go", Completed: false}, Todo{ID: "2", Task: "Eat dinner", Completed: false}, Todo{ID: "3", Task: "Go to sleep", Completed: false})
 
 	r := mux.NewRouter()
 
 	r.HandleFunc("/api/todos", getTodos).Methods("GET")
+	r.HandleFunc("/api/todos/{id}", getTodo).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":4000", r))
 }
