4c34965a8440f9cc32ca3820146b4c68c87f0a55
Brian Illes <brianrilles@gmail.com>
2019-04-20 20:23:02 -0700

    Adds POST /api/todos

--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -32,6 +33,15 @@ func getTodo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func postTodo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var todo Todo
+	json.NewDecoder(r.Body).Decode(&todo)
+	todo.ID = strconv.Itoa(len(todos) + 1)
+	todos = append(todos, todo)
+	json.NewEncoder(w).Encode(todo)
+}
+
 func main() {
 	todos = append(todos, Todo{ID: "1", Task: "Make an API with Go", Completed: false}, Todo{ID: "2", Task: "Eat dinner", Completed: false}, Todo{ID: "3", Task: "Go to sleep", Completed: false})
 
@@ -39,6 +49,7 @@ func main() {
 
 	r.HandleFunc("/api/todos", getTodos).Methods("GET")
 	r.HandleFunc("/api/todos/{id}", getTodo).Methods("GET")
+	r.HandleFunc("/api/todos", postTodo).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":4000", r))
 }
